2af07f59f9a77b19be988b239dc63ee4c6664004
Ali Kabiri <k4biri@icloud.com>
2023-02-13 14:26:37 +0100

    update gradle publishing plugin; fixed publish
    Signed-off-by: Ali Kabiri <k4biri@icloud.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ plugins {
     `java-gradle-plugin`
     id("org.jetbrains.kotlin.jvm") version "1.7.10"
     signing
-    id("com.gradle.plugin-publish") version "1.0.0"
+    id("com.gradle.plugin-publish") version "1.1.0"
     jacoco
     id("jacoco-report-aggregation")
 }
